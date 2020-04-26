f75a2c3c07f2f9f215677c2d99f7327594378875
Spencer Brower <foxtrotalphalima@gmail.com>
2020-04-25 21:21:10 -0400

    feat: Added Packer configurations.

--- provision.sh
+++ provision.sh
@@ -3,16 +3,16 @@
 # Installs the KDE Desktop environment
 
 # Update database
-apt-get update
+sudo apt-get update
 
 # Install KDE
-apt-get install -y kde-standard
+sudo apt-get install -y kde-standard puppet
 
 # Create sddm config directory
 SDDM_CONF=/etc/sddm.conf.d
 if [ ! -d $SDDM_CONF ]; then
     echo "Creating /etc/sddm.conf.d"
-    mkdir $SDDM_CONF
+    sudo mkdir $SDDM_CONF
 fi
 
 # Enable auto-login
@@ -21,5 +21,11 @@ if [ ! -f $AUTOLOGIN ]; then
     echo "Enabling auto-login"
     echo "[Autologin]
 User=vagrant
-Session=plasma.desktop" > $AUTOLOGIN
+Session=plasma.desktop" > /tmp/autologin.conf
+    sudo mv /tmp/autologin.conf $SDDM_CONF
 fi
+
+# Upgrade packages
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
+
+sudo reboot
